ef9a9ae386ff4ccc4d6150af9774fd510e093293
yinani24 <93968740+yinani24@users.noreply.github.com>
2023-06-17 23:42:59 +0000

    new

--- calhacks_2023/src/App.js
+++ calhacks_2023/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import microphone from './resources/microphone-icon.svg';
+import lady from './resources/interviewer.jpg';
+import useSpeechToText from 'react-hook-speech-to-text'; 
+import { useEffect } from 'react';
+//import axios from 'axios';
 
-function App() {
+export default function AnyComponent() {
+  const {
+    error,
+    interimResult,
+    isRecording,
+    results,
+    startSpeechToText,
+    stopSpeechToText,
+  } = useSpeechToText({
+    continuous: true,
+    useLegacyResults: false
+  });
+
+  // const handleSubmit = async (concatenatedResults) => {
+  //   try {
+  //     const response = await axios.post('/process-string', { data: concatenatedResults });
+  //     console.log(response.data);  // Handle the response from the server
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+
+  useEffect(() => {
+    let concatenatedResults = ""; // Initialize an empty string to hold the concatenated results
+    results.forEach((result) => {
+      concatenatedResults += `${result.transcript}. `; // Concatenate the `transcript` property
+    });
+    // handleSubmit(concatenatedResults);
+    console.log(concatenatedResults);
+  }, [isRecording]);
+  // const handleStop = () => {
+  //   console.log(interimResult) 
+  // };
+
+  if (error) return <p>Web Speech API is not available in this browser 🤷‍</p>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Recording: {isRecording.toString()}</h1>
+      <img src={lady} className="lady" alt="lady"/>
+      <button
+        onClick={isRecording ? stopSpeechToText : (startSpeechToText)}
+      >
+        <img src={microphone} alt="microphone"></img>
+        {isRecording ? 'Stop Recording' : 'Start Recording'}
+      </button>
+      
+      <ul>
+        {results.map((result) => (
+          <li key={result.timestamp}>{result.transcript}</li>
+        ))}
+        {interimResult && <li>{interimResult}</li>}
+      </ul>
+
     </div>
   );
 }
-
-export default App;
