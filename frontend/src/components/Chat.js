9aad4e174bad703ca3dde16b1d8f43df46c09cf9
VinnyXP <viph0009@gmail.com>
2023-06-18 08:11:55 -0700

    Final frontend

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -11,6 +11,10 @@ import TopNavBar from "./TopBar";
 
 const Chat = ({ room }) => {
   const [isPressed, setIsPressed] = useState(false);
+  const [isMicrophonePressed, setIsMicrophonePressed] = useState(false);
+  const [isCameraStarted, setIsCameraStarted] = useState(false);
+  const [isCameraStopped, setIsCameraStopped] = useState(true);
+  const [videoStream, setVideoStream] = useState(null);
   const {
     error,
     interimResult,
@@ -40,21 +44,59 @@ const Chat = ({ room }) => {
     handleSubmit(concatenatedResults);
     console.log(concatenatedResults);
   }, [isRecording]);
-  // const handleStop = () => {
-  //   console.log(interimResult) 
-  // };
 
-  //if (error) return <p>Web Speech API is not available in this browser 🤷‍</p>;
+  const videoRef = useRef();
 
-  const handleClick = () => {
-    setIsPressed(!isPressed);
+  const handleMicrophoneClick = () => {
+    setIsMicrophonePressed(!isMicrophonePressed);
   };
+
+  const handleStartCameraClick = () => {
+    setIsCameraStarted(true);
+    setIsCameraStopped(false);
+  };
+
+  const handleStopCameraClick = () => {
+    setIsCameraStarted(false);
+    setIsCameraStopped(true);
+  };
+
+
+  useEffect(() => {
+    const getVideoStream = async () => {
+      try {
+        if (isCameraStarted) {
+          const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+          setVideoStream(stream);
+        } else if (isCameraStopped) {
+          videoStream.getTracks().forEach((track) => track.stop());
+          setVideoStream(null);
+        }
+      } catch (error) {
+        console.error("Error accessing webcam:", error);
+      }
+    };
+
+    getVideoStream();
+
+    return () => {
+      if (videoStream) {
+        videoStream.getTracks().forEach((track) => track.stop());
+      }
+    };
+  }, [isCameraStarted, isCameraStopped]);
+
+
+  useEffect(() => {
+    if (videoStream && videoRef.current && !videoRef.current.srcObject) {
+      videoRef.current.srcObject = videoStream;
+    }
+  }, [videoStream]);
 
   return (
     <div>
-      <TopNavBar/>
+      <TopNavBar />
       <div className="container-fluid vh-100 d-flex flex-column p-3">
-        
         <div className="row flex-grow-1 mb-3">
           <div className="col-3">
             <div className="card bg-light p-3 h-100">
@@ -73,22 +115,22 @@ const Chat = ({ room }) => {
                 <img className="half-card-image" src={require('../assets/profilePicture2.jpeg')} alt="Profile Picture" />
               </div>
               <div className="d-flex align-items-center">
-                <button className="btn" onClick={handleClick}>
-                  {isRecording ? (
-                    <img src={require('../assets/redMicrophone.png')} alt="Red Microphone" style={{ width: '70%', height: '70%' }} /> , stopSpeechToText
+                <button className="btn" onClick={handleMicrophoneClick}>
+                  {isMicrophonePressed ? (
+                    <img src={require('../assets/redMicrophone.png')} alt="Red Microphone" style={{ width: '70%', height: '70%' }} />
                   ) : (
-                    <img src={require('../assets/blackMicrophone.png')} alt="Black Microphone" style={{ width: '70%', height: '70%' }} /> , startSpeechToText
+                    <img src={require('../assets/blackMicrophone.png')} alt="Black Microphone" style={{ width: '70%', height: '70%' }} />
                   )}
                 </button>
                 <div className="card bg-light p-3">
                   <div className="scrollable-text-inside">
                     <p className="card-text"></p>
-                      <ul>
-                        {results.map((result) => (
-                          <li key={result.timestamp}>{result.transcript}</li>
-                        ))}
-                        {interimResult && <li>{interimResult}</li>}
-                      </ul>
+                    <ul>
+                      {results.map((result) => (
+                        <li key={result.timestamp}>{result.transcript}</li>
+                      ))}
+                      {interimResult && <li>{interimResult}</li>}
+                    </ul>
                   </div>
                 </div>
               </div>
@@ -97,7 +139,17 @@ const Chat = ({ room }) => {
 
           <div className="col-3">
             <div className="card bg-light p-3 h-100">
-              <EmotionBars/> {/*parameter is emotions from app.py*/}
+              <EmotionBars /> {/*parameter is emotions from app.py*/}
+              {isCameraStarted && (
+                <div className="video-container">
+                  {videoStream && <video ref={videoRef} autoPlay muted playsInline width="100%" height="auto" style={{ objectFit: 'cover' }} />}
+                </div>
+              )}
+              {!isCameraStarted && (
+                <div className="not-started-card">
+                  <h5 className="card-title text-center word-wrap">Camera not started</h5>
+                </div>
+              )}
             </div>
           </div>
 
@@ -106,13 +158,33 @@ const Chat = ({ room }) => {
         <div className="row flex-grow-4 mt-3 pb-3">
           <div className="col">
             <div className="card bg-light p-3 h-100">
-              <div className="scrollable-text-bottom">
-                <p className="card-text">Etiam vitae velit quis mi luctus laoreet vel eget dui. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse potenti. Sed tincidunt sed arcu nec rhoncus. Curabitur eget purus enim. Proin commodo orci id sollicitudin accumsan. Integer a maximus purus. Sed ullamcorper dapibus neque, imperdiet lobortis nunc interdum id. Morbi id lacus vitae sapien abe mollis pellentesque. Curabitur at tellus dapibus, pretium felis et, sagittis mauris. Integer id tortor suscipit, ullamcorper ligula vitae, ornare turpis. Morbi eu dui eu massa faucibus blandit. Etiam a ullamcorper urna.
-                </p>
+              <div className="bottom-box d-flex justify-content-between p-1">
+                <div className="d-flex justify-content-between">
+                  <div><a className="btn btn-secondary" href="#" role="button">&lt;&lt; Previous Question</a></div>
+                  <div className="btn-margin-left"><a className="btn btn-secondary" href="#" role="button">Next Question &gt;&gt;</a></div>
+                </div>
+                {/* Uncomment if we want to readd middle buttons */}
+                {/* <div>
+                <a className="btn btn-secondary" href="#" role="button">Repeat Response</a>
+              </div>
+              <div>
+                <a className="btn btn-secondary" href="#" role="button">Return Home</a>
+              </div> */}
+                <div className="d-flex justify-content-between">
+                  <div>
+                    <button className="btn btn-secondary" onClick={handleStartCameraClick} disabled={isCameraStarted}>
+                      {isCameraStarted ? "Camera Started" : "Start Camera"}
+                    </button>
+                  </div>
+                  <div className="btn-margin-left">
+                    <button className="btn btn-secondary" onClick={handleStopCameraClick} disabled={isCameraStopped}>
+                      {isCameraStopped ? "Camera Stopped" : "Stop Camera"}
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
-
-          </div> 
+          </div>
         </div>
       </div>
     </div>
